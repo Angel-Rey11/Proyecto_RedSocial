9d7400c99c8313053753a903e5c38eb7baf78214
antonioajlb98 <antonioajlb98@gmail.com>
2022-11-15 01:24:22 +0100

    UserDao

--- src/main/java/com/iesfranciscodelosrios/Proyecto_RedSocial/Interfaces/IUserDAO.java
+++ src/main/java/com/iesfranciscodelosrios/Proyecto_RedSocial/Interfaces/IUserDAO.java
@@ -1,16 +1,19 @@
 package com.iesfranciscodelosrios.Proyecto_RedSocial.Interfaces;
 
+import com.iesfranciscodelosrios.Proyecto_RedSocial.model.DataObject.User;
+
 import java.util.List;
 
 public interface IUserDAO {
-    boolean create(IUserDAO u);
-    boolean delete(IUserDAO u);
-    boolean update(IUserDAO u);
-    IUserDAO find(IUserDAO u);
-    List<IUserDAO> getAllFollower();
-    List<IUserDAO> getAllFollowing();
-    boolean follow(IUserDAO u, IUserDAO u2);
-    boolean unfollow(IUserDAO u, IUserDAO u2);
-    boolean like(IUserDAO u, IPostDAO p);
-    boolean unlike(IUserDAO u, IPostDAO p);
+    boolean create(User u);
+    boolean delete(User u);
+    boolean update(User u);
+    List<User> getAllFollower();
+    List<User> getAllFollowing();
+    boolean follow(User u, User u2);
+    boolean unfollow(User u, User u2);
+    boolean like(User u, IPostDAO p);
+    boolean unlike(User u, IPostDAO p);
+    IUserDAO find(User u);
+
 }
