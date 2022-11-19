d50c5f3d37853a23629ba5b0d64d26ae1ae84147
franciscoberral <fberzaf1908@g.educaand.es>
2022-11-20 00:09:58 +0100

    Modificado

--- src/main/java/com/iesfranciscodelosrios/Proyecto_RedSocial/Assets/DataService.java
+++ src/main/java/com/iesfranciscodelosrios/Proyecto_RedSocial/Assets/DataService.java
@@ -6,6 +6,7 @@ import com.iesfranciscodelosrios.Proyecto_RedSocial.model.DAO.UserDAO;
 
 public class DataService {
 	public static UserDAO userLogeado = new UserDAO();
+	public static UserDAO uAux = new UserDAO();
 	public static PostDAO p = new PostDAO();
 	public static PostDAO pAux = new PostDAO();
 }
--- src/main/java/com/iesfranciscodelosrios/Proyecto_RedSocial/PerfilAuxController.java
+++ src/main/java/com/iesfranciscodelosrios/Proyecto_RedSocial/PerfilAuxController.java
@@ -2,11 +2,16 @@ package com.iesfranciscodelosrios.Proyecto_RedSocial;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import com.iesfranciscodelosrios.Proyecto_RedSocial.Assets.DataService;
 
 import com.iesfranciscodelosrios.Proyecto_RedSocial.model.DAO.FollowDAO;
+import com.iesfranciscodelosrios.Proyecto_RedSocial.model.DAO.PostDAO;
+import com.iesfranciscodelosrios.Proyecto_RedSocial.model.DAO.UserDAO;
+import com.iesfranciscodelosrios.Proyecto_RedSocial.model.DataObject.User;
+
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -26,11 +31,24 @@ public class PerfilAuxController implements Initializable{
 	private Button unfollow;
 	@FXML
 	private TextArea bio;
+	@FXML 
+	private Label nFollower;
+	@FXML
+	private Label nFollowing;
+	@FXML 
+	private Label nPost;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
+		
 		nickname.setText(DataService.pAux.getUser().getNickname());
 		bio.setText(DataService.pAux.getUser().getBiografia());
+		User pDAO = DataService.pAux.getUser();
+		DataService.uAux = (UserDAO) pDAO;
+		nFollower.setText(String.valueOf(DataService.uAux.getAllFollower().size()));
+		nFollowing.setText(String.valueOf(DataService.uAux.getAllFollowing().size()));
+		List<PostDAO> listPost = PostDAO.getPostsByUser(DataService.uAux.getId());
+		nPost.setText(String.valueOf(listPost.size()));
 	}
 	
 	@FXML
